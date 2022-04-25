548fe1ee5f3c6d240eaad0de18511b65654c4aec
Joseph-S12 <jjsisson14@gmail.com>
2022-04-25 15:57:57 +0100

    Ignore the test-reports directory

--- headless/src/main/java/com/engteam14/yorkpirates/headless/AssetTests.java
+++ headless/src/main/java/com/engteam14/yorkpirates/headless/AssetTests.java
@@ -5,18 +5,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.assertTrue;
 
+/** This is a class to test that ALL assets required by the game exist. */
 @RunWith(HeadlessLauncher.class)
 public class AssetTests {
 
+    /** Tests if the alcuin_2.png file exists */
     @Test
     public void testAlcuin2AssetExists() {
-        assertTrue("This test will only pass when the ship.png asset exists.", Gdx.files
+        assertTrue("This test will only pass when the alcuin_2.png asset exists.", Gdx.files
                 .internal("alcuin_2.png").exists());
     }
 
+    /** Tests if the alcuin_boat.png file exists */
     @Test
     public void testAlcuinBoatAssetExists() {
-        assertTrue("This test will only pass when the ship.png asset exists.", Gdx.files
+        assertTrue("This test will only pass when the alcuin_boat asset exists.", Gdx.files
                 .internal("alcuin_boat.png").exists());
     }
 }
