a3208ea552adb3f2b644b789e0b9713873033b76
Joseph-S12 <jjsisson14@gmail.com>
2022-05-03 20:56:36 +0100

    Revert "Revert "Added test class for GameScreen. Modified test for Colleges""
    This reverts commit 87ba0fa082d3609b7b06a0e74cfc9fbdffbf71cd.

---
+++ headless/src/main/java/com/engteam14/yorkpirates/headless/GameScreenTests.java
@@ -0,0 +1,26 @@
+package com.engteam14.yorkpirates.headless;
+
+import com.badlogic.gdx.Gdx;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Array;
+
+import com.engteam14.yorkpirates.College;
+import com.engteam14.yorkpirates.Player;
+
+/** This is a class to test that as much of the GameScreen code as possible, works. */
+@RunWith(HeadlessLauncher.class)
+public class GameScreenTests {
+
+
+  @Test
+  public void testScreenInit() throws Exception {
+
+  }
+
+
+}
