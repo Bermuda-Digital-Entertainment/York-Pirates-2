87ba0fa082d3609b7b06a0e74cfc9fbdffbf71cd
Xisho <sds543@york.ac.uk>
2022-05-03 18:24:37 +0100

    Revert "Added test class for GameScreen. Modified test for Colleges"
    This reverts commit 4031aa3104ffb1d560de1a4cf4d816c65ebe62af.

--- headless/src/main/java/com/engteam14/yorkpirates/headless/CollegeTests.java
+++ headless/src/main/java/com/engteam14/yorkpirates/headless/CollegeTests.java
@@ -12,26 +12,20 @@ import com.badlogic.gdx.utils.Array;
 import com.engteam14.yorkpirates.College;
 import com.engteam14.yorkpirates.Player;
 
-/** This is a class to test that the College class is working correctly. */
+/** This is a class to test that ALL assets required by the game exist. */
 @RunWith(HeadlessLauncher.class)
 public class CollegeTests {
 
-  protected College createCollege(String name, String team){
+  @Test
+  public void testCollegeInit() throws Exception {
     Array<Texture>testTexture = new Array<>();
     Array<Texture>testPlayerTexture = new Array<>();
     Player player;
     College testObject;
+
     testPlayerTexture.add(new Texture("ship1.png"));
-    player = new Player(testPlayerTexture, 0, 30f, 35f, 32f, 16f, "PLAYER");
+    player = new Player(testPlayerTexture, 0, 30f, 35f, 32f, 16f, "red");
     testTexture.add(new Texture("alcuin_2.png"));
-    testObject = new College(testTexture, 12, 10, 1, name, team, player);
-    return testObject;
+    testObject = new College(testTexture, 12, 10, 1, "TESTCOLLEGEPLEASEIGNORE", "red", player);
   }
-
-  @Test
-  public void testCollegeInit() throws Exception {
-    College testCollege = createCollege("TestCollegePleaseIgnore", "Red");
-  }
-
-
 }
